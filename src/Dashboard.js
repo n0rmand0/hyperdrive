cd3515bbb516e943de7b5d57fc74f53079488c00
Ricky Normandeau <ricky.normandeau@pnc.com>
2018-04-19 14:34:01 -0400

    variables, colors, etc

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import pin from './images/pin.png';
 
 class Dashboard extends Component {
   constructor(props) {
@@ -34,20 +35,43 @@ class Dashboard extends Component {
 
         <div className="dashboard__section">
           <div className="l-container">
-            <article className="dashboard__info">
+
+          <div className="dashboard__overview-container">
+            <article className="dashboard__overview">
               <h3>Project</h3>
-              <h2 className="dashboard__info__id">XXX1234567</h2>
+              <h2 className="dashboard__overview__id">XXX1234567</h2>
             </article>
-            <article className="dashboard__info">
+            <article className="dashboard__overview">
               <h3>Description</h3>
               <p>Lorem ipsum dolor ...</p>
             </article>
+            <article className="dashboard__overview">
+              <h3>Progress</h3>
+              <p>50/100 Tasks complete</p>
+              <span className="tag">On Track</span>
+            </article>
+          </div>
+
+            <div className="timeline">
+              <div className="timeline__pin">
+                <img src={pin} alt="pin" />
+              </div>
+              <div className="timeline__progress">
+                <div className="timeline__progress__bar"></div>
+              </div>
+            </div>
           </div>
         </div>
 
         <div className="dashboard__section">
           <div className="l-container">
-              <h3>Project Overview</h3>
+              <h3>Project Breakdown</h3>
+              <div className="dashboard__grid" style={{'--status-color': 'var(--red)'}}>
+                <h4 className="dashboard__grid__title">Initiative Planning</h4>
+                <div className="dashboard__grid__block">Task</div>
+                <div className="dashboard__grid__block">Task</div>
+                <div className="dashboard__grid__block">Task</div>
+              </div>
               <div className="dashboard__grid">
                 <h4 className="dashboard__grid__title">Planning</h4>
                 <div className="dashboard__grid__block">Task</div>
@@ -56,12 +80,6 @@ class Dashboard extends Component {
               </div>
               <div className="dashboard__grid">
                 <h4 className="dashboard__grid__title">Executing</h4>
-                <div className="dashboard__grid__block">Task</div>
-                <div className="dashboard__grid__block">Task</div>
-                <div className="dashboard__grid__block">Task</div>
-              </div>
-              <div className="dashboard__grid">
-                <h4 className="dashboard__grid__title">Closing</h4>
                 <div className="dashboard__grid__block">Task</div>
                 <div className="dashboard__grid__block">Task</div>
                 <div className="dashboard__grid__block">Task</div>
@@ -99,7 +117,7 @@ class Dashboard extends Component {
         <div className="dashboard__section">
           <div className="l-container">
             <div className="l-short">
-              <h3>Failed</h3>
+              <h3>Completed</h3>
               <div className="dashboard__task">
                 <h5>Task</h5>
                 <p>Lorem ipsum dolor amet...</p>
